cafe0029b319f3be28192b47ea6ec10e06d4da03
Rajesh <rajesh@Rajeshs-Mac.local>
2019-01-09 00:57:14 -0800

    full layout for ipad and iphone

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        devLogo.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
